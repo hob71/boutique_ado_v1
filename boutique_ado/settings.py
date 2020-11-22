3f12eff5b863d792271b7bde893de253612cb327
hob71 <roberthobday@sky.com>
2020-11-22 09:49:56 +0000

    Added checkout and payment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # other
-    'crispy-forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -176,4 +176,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-
+STRIPE_WH_KEY = os.getenv('STRIPE_WH_KEY', '')
